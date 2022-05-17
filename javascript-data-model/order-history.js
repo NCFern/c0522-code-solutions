d06c91298d648596250aadbeacb459cfbeda63fb
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-17 19:52:00 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,77 @@
+var orderHistory = [
+  {
+    title: 'JavaScript for Impatient Programmers',
+    author: 'Dr. Axel Rauschmayer',
+    cost: '31.55',
+    orderPlaced: 'August 4, 2020',
+    delivered: 'August 8, 2020',
+    returnBy: 'September 7, 2020',
+    orderNumber: '114-3941689-8772232',
+    type: 'book',
+    shipTo: 'JS Masher',
+    handedTo: 'N/A',
+    groupOrder: '4'
+  },
+
+  {
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+    cost: '41.33',
+    orderPlaced: 'July 19, 2020',
+    delivered: 'July 20, 2020',
+    returnBy: 'August 19, 2020',
+    orderNumber: '113-9984268-1280257',
+    type: 'book',
+    shipTo: 'JS Masher',
+    handedTo: 'N/A',
+    groupOrder: '3'
+  },
+
+  {
+    title: 'GameCube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: 'N/A',
+    cost: '15.98',
+    orderPlaced: 'July 4, 2020',
+    delivered: 'July 7, 2020',
+    returnBy: 'August 5, 2020',
+    orderNumber: '114-2875557-9059409',
+    type: 'electronic',
+    shipTo: 'JS Masher',
+    handedTo: 'resident',
+    groupOrder: '2'
+  },
+
+  {
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    author: 'N/A',
+    cost: '94.95',
+    orderPlaced: 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    returnBy: 'August 4, 2020',
+    orderNumber: '113-2883177-2648248',
+    type: 'electronic',
+    shipTo: 'JS Masher',
+    handedTo: 'resident',
+    groupOrder: '1'
+  },
+
+  {
+    title: 'The Art of Sql',
+    author: 'Stephane Faroult',
+    cost: '33.99',
+    orderPlaced: 'July 3, 2020',
+    delivered: 'July 5, 2020',
+    returnBy: 'August 4, 2020',
+    orderNumber: '113-2883177-2648248',
+    type: 'book',
+    shipTo: 'JS Masher',
+    handedTo: 'N/A',
+    groupOrder: '1'
+  },
+
+  {
+    orderTotalPrice: ['138.93', '17.22', '44.53', '31.55'],
+    orderTotalItems: ['2', '1', '1', '1']
+  }
+
+];
