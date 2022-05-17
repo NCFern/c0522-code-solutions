c561cb7fc580a43b6544e2d1b79783d870c81b43
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-17 00:47:10 +0000

    completed javascript-operators-and-expressions

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -1,18 +1,18 @@
-var width = '867530.9';
-var height = '10';
+var width = 867530.9;
+var height = 10;
 var area = width * height;
 console.log('value of area:', area);
 console.log('type of area:', typeof area);
 
-var bill = '580';
-var payment = '1000';
+var bill = 580;
+var payment = 1000;
 var change = payment - bill;
 console.log('value of change:', change);
 console.log('type of change:', typeof change);
 
-var quizzes = '81';
-var midterm = '49';
-var final = '79';
+var quizzes = 81;
+var midterm = 49;
+var final = 79;
 var grade = (quizzes * 0.25) + (midterm * 0.35) + (final * 0.4);
 console.log('value of grade:', grade);
 console.log('type of grade:', typeof grade);
@@ -23,13 +23,13 @@ var fullName = firstName + ' ' + lastName;
 console.log('value of fullName:', fullName);
 console.log('type of fullName:', typeof fullName);
 
-var pH = '4';
+var pH = 4;
 var isAcidic = (pH < 7);
 console.log('value of isAcidic:', isAcidic);
 console.log('type of isAcidic:', typeof isAcidic);
 
-var headCount = '300';
-var isSparta = headCount === '300';
+var headCount = 300;
+var isSparta = headCount === 300;
 console.log('value of isSparta', isSparta);
 console.log('type of isSparta', typeof isSparta);
 
