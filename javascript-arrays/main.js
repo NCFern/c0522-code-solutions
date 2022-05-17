c4a7cee00534a877f1ae0ea0f3c1204f2d4c725c
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-17 06:44:52 +0000

    in progress

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -1,3 +1,40 @@
-var colors = ['red', 'white', 'blue'];
+var colors = [
+  'red',
+  'white',
+  'blue'
+];
 
-console.log('value of colors:', colors);
+console.log('value of colors[0]', colors[0]);
+console.log('value of colors[1]', colors[1]);
+console.log('value of colors[2]', colors[2]);
+
+var America = colors[0] + ' ' + colors[1] + ' ' + 'and' + ' ' + colors[2];
+console.log('America is', America);
+
+colors[2] = 'green';
+
+var Mexico = colors[0] + ' ' + colors[1] + ' ' + 'and' + ' ' + colors[2];
+console.log('Mexico is', Mexico);
+
+console.log('colors', colors);
+
+var students = [
+  'Leonardo',
+  'Donatello',
+  'Raphael',
+  'Michaelangelo'
+];
+
+var numberOfStudents;
+numberOfStudents = students.length;
+
+console.log('There are', numberOfStudents);
+
+var lastIndex;
+lastIndex = numberOfStudents - 1;
+
+var lastStudent;
+lastStudent = lastIndex;
+console.log('The last student in the array is', lastStudent);
+
+console.log('students', students);
