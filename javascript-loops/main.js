a98b5ad82f5a5317390b90c4c2fb504b7148f091
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-19 06:13:45 +0000

    in progress

---
+++ javascript-loops/main.js
@@ -0,0 +1,11 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+for (var x = 0; x < 10; x++) {
+  console.log(x);
+}
