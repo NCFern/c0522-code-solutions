4821acd51df99ee73f8cd632345eec5758389e82
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-19 07:28:45 +0000

    in progress

--- javascript-functions-practice/get-initials-of-person.js
+++ javascript-functions-practice/get-initials-of-person.js
@@ -1,1 +1,5 @@
 /* exported getInitialsOfPerson */
+function getInitialsOfPerson(person) {
+  var bothInitials = ((person.firstName.charAt(0)) + (person.lastName.charAt(0)));
+  return bothInitials;
+}
