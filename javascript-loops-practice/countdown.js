2ab1efac215bebbafc638cd576de4bbfaeb819cf
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-20 17:03:18 +0000

    javascript-loops-practice

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,8 +1,9 @@
 /* exported countdown */
-function countdown(number) {
+function countdown(number) { // function being named countdown with the parameter number (some interger)
   var numberArray = []; // create array for output //
-  var currentNumber = numberArray;// makes our number equal our current number//
-  for (var i = currentNumber; i > 0; i--) {
-    if (currentNumber === 0);
+  while (number >= 0) { // while number is greater than 0, we run the code block//
+    numberArray.push(number); // we push the current number into the number array//
+    number--;// replace number with number - 1//
   }
+  return numberArray; // return the numbers in the bracket to the countdown//
 }
--- javascript-loops-practice/sum-all.js
+++ javascript-loops-practice/sum-all.js
@@ -1,1 +1,12 @@
 /* exported sumAll */
+function sumAll(numbers) { // function being named sumAll with parameter numbers (ex-1,2,4)
+  var outputNumber = 0; // initially start with 0 //
+  for (var i = 0; i < numbers.length; i++) {
+    // when i is less than the length property of the numbers object //
+    outputNumber = numbers[i] + outputNumber;
+  } // numbers[0] + numbers[1] + numbers[2] + ...//
+  return outputNumber;
+}
+// [ex-1,3,7]
+// while we have numbers in our array,we continue to add it to our current number//
+// push the final output//
