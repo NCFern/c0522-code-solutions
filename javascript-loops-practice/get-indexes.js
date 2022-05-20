77922c728cc26bffe718ca1ebcdcae801ac656ae
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-20 20:25:11 +0000

    in progress

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,8 +1,10 @@
 /* exported getIndexes */
 function getIndexes(array) {
-  var finalIndex = 0;
+  var finalIndex = [];
   for (var i = 0; i < array.length; i++) {
-    finalIndex = array[i] + finalIndex;
+    // while i is less than the array length, we run the code block//
+    // then add 1 each iteration //
+    finalIndex.push(i); // push the array number onto our final index//
   }
   return finalIndex;
 }
--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -1,1 +1,11 @@
 /* exported getStudentNames */
+// access array for student.name at start index[0]//
+// push student.name[0] into output into array//
+// go back to start. keep going until i < student.name.length //
+
+function getStudentNames(students) {
+  var finalStudents = [];
+  for (var i = 0; i < students.length; i++); // use for...in loop //
+
+  return finalStudents;
+}
