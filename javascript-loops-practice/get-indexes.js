5c9102fae8eb7c96374bb612378bfc1eb481747d
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-20 18:27:49 +0000

    in progress

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,1 +1,8 @@
 /* exported getIndexes */
+function getIndexes(array) {
+  var finalIndex = 0;
+  for (var i = 0; i < array.length; i++) {
+    finalIndex = array[i] + finalIndex;
+  }
+  return finalIndex;
+}
