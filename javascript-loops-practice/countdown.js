8585ec4ac052e8cfba1e95bc8e16533545cc3b94
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-20 01:08:06 +0000

    in progress

---
+++ javascript-loops-practice/countdown.js
@@ -0,0 +1,8 @@
+/* exported countdown */
+function countdown(number) {
+  var numberArray = []; // create array for output //
+  var currentNumber = numberArray;// makes our number equal our current number//
+  for (var i = currentNumber; i > 0; i--) {
+    if (currentNumber === 0);
+  }
+}
