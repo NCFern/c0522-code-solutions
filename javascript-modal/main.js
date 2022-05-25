921a0c53526fbdcc76077e335d8534957fd9e884
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-25 05:17:28 +0000

    in progress

---
+++ javascript-modal/main.js
@@ -0,0 +1,15 @@
+// use doc.querySelector to get modal button in html and assign to var //
+// have var userClicks that stores amount of clicks //
+// create a function openModal that opens pop-up //
+// div class empty to div class pop-up //
+// in pop-up, have button that exits out of modal //
+// everytime button is clicked, start the function //
+//  //
+
+var buttonModal = document.querySelector('.modal');
+
+function openModal(event) {
+// code block stuffs //
+}
+
+buttonModal.addEventListener('click', openModal);
