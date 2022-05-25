f494563655b7a88ffe2218b296218e05f19eebcf
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-25 17:47:22 +0000

    in progress

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,22 @@
+// function handleFocus(event) {
+//   console.log('focus event fired', handleFocus);
+//   console.log(event.target.name);
+// }
+
+// function handleBlur(event) {
+//   console.log('blur event fired', handleBlur);
+//   console.log(event.target.name);
+// }
+
+// function handleInput(event) {
+//   console.log(event.target.name);
+//   console.log(event.target.value);
+// }
+
+// var formName = document.querySelector('.user-name');
+// var formEmail = document.querySelector('.user-email');
+// var formMessage = document.querySelector('.user-message');
+
+// formName.addEventListener('focus', handleFocus);
+// formEmail.addEventListener('blur', handleBlur);
+// formMessage.addEventListener('input', handleInput);
