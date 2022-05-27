fb6330b1b3ebb0cfb7ade499a38bbe3e2a1b8602
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-27 00:47:49 +0000

    in progress

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,33 @@
+// query all of tab and view elements //
+// click event listener on .tab-container //
+// click event listener to parent element //
+// if statement to see if event.target === matches(.'tab') //
+// make loop of node list of tab elements and compare with event.target //
+// update event.target to include .active for active tab //
+
+// var $tabContainer = document.querySelector('.tab-container');
+
+// var $tabLanguage1 = document.querySelector('.tab.active');
+// var $tabLanguage2 = document.querySelector('.tab');
+
+// var $viewContainer = document.querySelector('.view-container');
+
+// var $viewSeen = document.querySelector('.view');
+// var $viewHidden = document.querySelector('.view hidden');
+
+// $tab-container.addEventListener('click', switchTabs);
+// if (event.target(.tab)) {
+// }
+
+// var $userClicked = true;
+
+// function switchTabs(event) {
+//   userClicked = !userClicked;
+//   if (userClicked) {
+//     tabLanguage1.className = 'tab-active';
+//     viewSeen.className = 'view';
+//   } else {
+//     tabLanguage2.className = 'tab';
+//     viewHidden.className = 'view-hidden';
+//   }
+// }
