9f8832ef8de74d23e7d0ad79faca5bd5685e324d
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-28 07:32:50 +0000

    in progress

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,18 +1,18 @@
 /* exported isUpperCased */
 // write a function that checks if the word is uppercased //
-// create a var that checks the character at string 0 if its capitalized//
+// create a var that checks if entire word is capitalized//
 // if yes, return true //
 // if no, return false //
 // return output //
 
-function isUpperCased(word) {
-  var capital = [];
+// function isUpperCased(word) {
+//   var capital = [];
 
-  if (word !== word.charAt(0).toUpperCase()) {
-    return false;
-  }
-  if (word !== word.charAt(0).toLowerCase()) {
-    return true;
-  }
-  return capital;
-}
+//   if (word !== word.charAt(0).toUpperCase()) {
+//     return false;
+//   }
+//   if (word !== word.charAt(0).toLowerCase()) {
+//     return true;
+//   }
+//   return capital;
+// }
