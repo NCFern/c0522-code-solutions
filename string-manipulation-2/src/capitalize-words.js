7fb4cdf1c0d3e6631d331da09d40f314bee576ec
Nicholas Fernandez <Nickfer610@gmail.com>
2022-06-08 04:55:55 +0000

    completed-string-manipulation-2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -4,13 +4,19 @@
 // make new variable of empty string //
 // upper case first letter of string //
 // make for loop that starts at index 1 for string and lower case the rest of word //
-// if statement to help us go through other strings in array //
+// if statement to detect if there is a space//
+// if so, capitalize the string[i + 1] //
 
 function capitalizeWords(string) {
   var capitalString = '';
   capitalString = string[0].toUpperCase();
   for (var i = 1; i < string.length; i++) {
     capitalString += string[i].toLowerCase();
+
+    if (string[i] === ' ') {
+      capitalString += string[i + 1].toUpperCase();
+      i++;
+    }
   }
   return capitalString;
 }
