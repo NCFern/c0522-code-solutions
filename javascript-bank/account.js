428cfffa844d232a3b9efbd7c0a1900a4c03e05e
Nicholas Fernandez <Nickfer610@gmail.com>
2022-06-09 05:19:03 +0000

    completed javascript-bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -5,7 +5,15 @@
 // if statement that if number is not a number, is > 0, not an interger, is NaN, return false //
 // new var for new transaction //
 // push new transcation and return as true //
-// Account.prototype.withdraw, repeat as above//
+// Account.prototype.withdraw
+// repeat as above//
+// Account.prototype.getBalance //
+// make vars for deposit and withdraw equaling 0 //
+// for loop to go through transactions array //
+// if statement to see if transaction type is deposit //
+// if yes, add it to deposit amount //
+// if no, add it to withdraw amoount //
+// return statement of deposits - withdraw //
 
 function Account(number, holder) {
   this.number = number;
@@ -34,5 +42,13 @@ Account.prototype.withdraw = function (amount) {
 Account.prototype.getBalance = function () {
   var finalDeposits = 0;
   var finalWithdrawls = 0;
+
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      finalDeposits += this.transactions[i].amount;
+    } else {
+      finalWithdrawls += this.transactions[i].amount;
+    }
+  }
   return finalDeposits - finalWithdrawls;
 };
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,7 +1,50 @@
 /* exported Bank */
+// Bank.prototype.openAccount //
+// if balance > 0 and and interger (balance % 1 === 0), make new acc object (this.nextAccountNumber, holder) //
+// call deposit prototype to deposit balance //
+// push new acc int array then increment the next acc //
+// return new acc number //
+// if not, return null //
+// Bank.prototype.getAccount //
+// for loop to go through accounts array and see if (this.accounts[i].number === number) //
+// if yes, return this.accounts[i]; //
+// if not, return null //
+// Bank.prototype.getTotalAssets //
+// make var assets = 0 to represent total assets //
+// for loop to go through account array //
+// add sum of accounts by calling getBalance //
+// return assets //
 
 function Bank() {
   this.nextAccountNumber = 1;
   this.accounts = [];
-
 }
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && balance % 1 === 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var assets = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    assets += this.accounts[i].getBalance();
+  }
+  return assets;
+};
