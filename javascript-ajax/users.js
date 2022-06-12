5512b1c64a0aa905400184153b44c8bfca6f18ce
Nicholas Fernandez <Nickfer610@gmail.com>
2022-06-12 22:56:11 +0000

    completed javascipt-ajax

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -7,31 +7,11 @@ xhr.addEventListener('load', function () {
   console.log(xhr.status);
   console.log(xhr.response);
 
-  var users = xhr.response;
-  for (var i = 0; i < xhr.length; i++) {
-    var user = users[i];
+  for (var i = 0; i < xhr.response.length; i++) {
     var $li = document.createElement('li');
-    $li.textContent = user;
+    $li.textContent = xhr.response[i].name;
     $userList.appendChild($li);
   }
 });
 
 xhr.send();
-
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-// xhr.responseType = 'json';
-// xhr.addEventListener('load', function () {
-//   console.log(xhr.status);
-//   console.log(xhr.response);
-
-//   for (var i = 0; i < xhr.length; i++) {
-//     $userList.push(xhr.name[i]);
-//   }
-
-//   var $userName = document.createElement('li');
-//   $userName.textContent = xhr.name;
-
-//   $userList.appendChild($userName);
-//   xhr.send();
-// });
